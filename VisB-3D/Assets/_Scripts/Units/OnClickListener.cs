a571fe63164814e35b9debd1713a9501af772c9d
DerHyper <maxrichter15@web.de>
2025-08-10 22:24:32 +0200

    Add OnClickManager

---
+++ VisB-3D/Assets/_Scripts/Units/OnClickListener.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickListener : MonoBehaviour
+{
+    /// <summary>
+    /// This method is called by the <c>InputManager</c> when a <c>GameObject<c> with a <c>OnClickListener</c> is clicked on.
+    /// </summary>
+    public void Click()
+    {
+        Debug.Log("Click on " + gameObject.name + " registered");
+    }
+}
