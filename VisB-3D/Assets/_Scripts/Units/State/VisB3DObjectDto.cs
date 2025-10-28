357bc5e5049ae19b985251a334bf17234097607b
DerHyper <maxrichter15@web.de>
2025-10-28 20:45:34 +0100

    Fix VisB3DObjectDto serializability issues

--- VisB-3D/Assets/_Scripts/Units/State/VisB3DObjectDto.cs
+++ VisB-3D/Assets/_Scripts/Units/State/VisB3DObjectDto.cs
@@ -6,12 +6,12 @@ public class VisB3DObjectDto
 {
     public string name;
     public string material;
-    public bool isActive;
-    public Vector3 position;
-    public Vector3 rotation;
-    public Vector3 scale;
+    public bool? isActive;
+    public Vector3Dto position;
+    public Vector3Dto rotation;
+    public Vector3Dto scale;
 
-    public VisB3DObjectDto(string name, string material, bool isActive, Vector3 position, Vector3 rotation, Vector3 scale)
+    public VisB3DObjectDto(string name, string material, bool? isActive, Vector3Dto position, Vector3Dto rotation, Vector3Dto scale)
     {
         this.name = name;
         this.material = material;
