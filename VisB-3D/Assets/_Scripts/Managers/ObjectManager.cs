3f59214e7e3063ba85f5e514f059244b1503bee6
DerHyper <maxrichter15@web.de>
2025-10-29 20:14:50 +0100

    Add object vis updating to ObjectManager

--- VisB-3D/Assets/_Scripts/Managers/ObjectManager.cs
+++ VisB-3D/Assets/_Scripts/Managers/ObjectManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -9,6 +10,8 @@ public class ObjectManager : MonoBehaviour
     public static ObjectManager instance;
     public GameObject myObject;
     public TMP_InputField messageOutputField;
+    public GameObject stateObjectParent;
+    public Dictionary<string, StateUpdater> managedObjects = new();
 
     private void Awake()
     {
@@ -20,6 +23,13 @@ public class ObjectManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void Start()
+    {
+        stateObjectParent.GetComponentsInChildren<StateUpdater>().ToList().ForEach(su =>
+            managedObjects.Add(su.gameObject.name, su)
+        );
     }
 
     public void IncPosition()
@@ -35,5 +45,12 @@ public class ObjectManager : MonoBehaviour
     public void UpdateVisualization(string data)
     {
         VisB3DDto dto = MessageParser.MessageToState(data);
+        foreach (VisB3DObjectDto objDto in dto.objectStates)
+        {
+            if (managedObjects.TryGetValue(objDto.name, out StateUpdater stateUpdater))
+            {
+                stateUpdater.UpdateState(objDto);
+            }
+        }
     }
 }
