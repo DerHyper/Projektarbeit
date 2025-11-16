b66a66d6859a672d64203b30a26b5cbb2c7f0291
DerHyper <maxrichter15@web.de>
2025-11-16 21:49:23 +0100

    Add GameManager

---
+++ VisB-3D/Assets/_Scripts/Managers/GameManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public AppState currentState = AppState.Idle;
+    public enum AppState
+    {
+        Idle,
+        LoadingModel,
+        Running,
+        Error
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetState(AppState newState)
+    {
+        currentState = newState;
+        Debug.Log("Game state changed to: " + newState.ToString());
+    }
+
+    public async void LoadModel(string uri)
+    {
+        SetState(AppState.LoadingModel);
+        bool success = await ModelLoadingManager.instance.LoadModelFromUri(uri);
+        if (success)
+        {
+            ModelLoadingManager.instance.InitModelObjects();
+        }
+        SetState(AppState.Running);
+    }
+}
