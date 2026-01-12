1c37206f818b9eae14d362dc8d092ba313296d64
DerHyper <maxrichter15@web.de>
2026-01-12 16:25:38 +0100

    Refactor ObjectManager to support special forms

--- VisB-3D/Assets/_Scripts/Managers/ObjectManager.cs
+++ VisB-3D/Assets/_Scripts/Managers/ObjectManager.cs
@@ -50,15 +50,43 @@ public class ObjectManager : MonoBehaviour
         messageOutputField.text = text;
     }
 
+/// <summary>
+/// Destroy the object with the given name and remove it from managedObjects
+/// </summary>
+/// <param name="name"></param>
+    public void DestroyObject(string name)
+    {
+        if (managedObjects.TryGetValue(name, out StateUpdater stateUpdater))
+        {
+            Destroy(stateUpdater.gameObject);
+            managedObjects.Remove(name);
+        }
+    }
+
+    /// <summary>
+    /// Update the visualization based on incoming data
+    /// </summary>
+    /// <param name="data"></param>
     public void UpdateVisualization(string data)
     {
+        SpecialFormManager.OnStartVisualizationUpdate();
         VisB3DDto dto = MessageParser.MessageToState(data);
         foreach (VisB3DObjectDto objDto in dto.objectStates)
         {
-            if (managedObjects.TryGetValue(objDto.name, out StateUpdater stateUpdater))
-            {
-                stateUpdater.UpdateState(objDto);
-            }
+            SpecialFormManager.TryAddSpecialForm(objDto);
+            CheckStateUpdater(objDto);
+        }
+    }
+
+    /// <summary>
+    /// Check if there is a StateUpdater for the object and update its state
+    /// </summary>
+    /// <param name="objDto"></param>
+    private void CheckStateUpdater(VisB3DObjectDto objDto)
+    {
+        if (managedObjects.TryGetValue(objDto.name, out StateUpdater stateUpdater))
+        {
+            stateUpdater.UpdateState(objDto);
         }
     }
 }
